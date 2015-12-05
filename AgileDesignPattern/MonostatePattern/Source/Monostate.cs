829ba0970875abc106234c09676af17e7f983030
liupanpan <liupanpan@oneapm.com>
2015-12-05 14:21:48 +0800

    Monostate Pattern

---
+++ AgileDesignPattern/MonostatePattern/Source/Monostate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonostatePattern.Source
+{
+    public class Monostate
+    {
+        private static int _itsX = 0;
+
+        public int ItsX
+        {
+            get
+            {
+                return _itsX;
+                
+            }
+            set
+            {
+                _itsX = value;
+            }
+        }
+
+        public Monostate(int value = 0)
+        {
+            ItsX = value;
+        }
+    }
+}
---
+++ AgileDesignPattern/MonostatePattern/Test/TestMonostate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MonostatePattern.Source;
+using NUnit.Framework;
+
+namespace MonostatePattern.Test
+{
+    [TestFixture]
+    class TestMonostate
+    {
+        [Test]
+        public void TestMonostateUnique()
+        {
+            Monostate obj1 = new Monostate();
+            Monostate obj2 = new Monostate();
+
+            Assert.AreEqual(obj1.ItsX, obj2.ItsX);
+        }
+
+        [Test]
+        public void TestMonostateUnique2()
+        {
+            Monostate obj1 = new Monostate();
+            Monostate obj2 = new Monostate();
+            obj1.ItsX = 10;
+            Assert.AreEqual(obj2.ItsX, 10);
+        }
+    }
+}
